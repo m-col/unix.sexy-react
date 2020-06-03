0cfed504f894092fff758f61bbc222bc07c24265
mcol <mcol@posteo.net>
2020-06-03 20:46:10 +0100

    persist store in localStorage

--- src/store.js
+++ src/store.js
@@ -1,8 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit';
-import backgroundReducer from './features/background';
+import { createStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
 
-export default configureStore({
-  reducer: {
+import backgroundReducer from './features/background';
+import windowReducer from './features/windows/slice';
+
+
+const reducer = combineReducers({
     background: backgroundReducer,
-  },
-});
+    windows: windowReducer,
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducer)
+
+export const store = createStore(persistedReducer);
+export const persistor = persistStore(store);
